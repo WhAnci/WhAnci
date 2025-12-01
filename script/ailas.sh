c7e9fe97a69bd90a9684f04c20adad7694d6674c
WhAnci <leeshiny7@icloud.com>
2025-12-01 17:20:54 +0900

    Create ailas.sh

---
+++ script/ailas.sh
@@ -0,0 +1,14 @@
+# Git Auto Push
+g() {
+  if [ -z "$1" ]; then
+    echo "사용법: g '커밋메시지' [브랜치명]"
+    return 1
+  fi
+  
+  local commit_msg="$1"
+  local branch="${2:-$(git branch --show-current)}"
+  
+  git add -A && \
+  git commit -m "$commit_msg" && \
+  git push origin "$branch"
+}
